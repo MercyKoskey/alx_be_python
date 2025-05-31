7b670cec7a848ed0d7996a2bc5874df7c76aa08d
Mercy Koskey <koskey.mercy@gmail.com>
2025-05-31 10:14:23 +0300

    using operations

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,7 +3,7 @@ print("simple calculator")
 #get inputs from the user
 number1 = float(input("Enter the first number:"))
 number2 = float(input("Enter the second number:"))
-operation = (input("Choose the operation (+, -, *, /):."))
+operation = input("Choose the operation (+, -, *, /): ")
 #Perform the Calculation Using Match-Case
 match operation:
     case "+":
