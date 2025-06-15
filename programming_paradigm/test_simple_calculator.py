785cb879ff8cb3d92de4186c9d0c87ec733e9973
Mercy Koskey <koskey.mercy@gmail.com>
2025-06-15 22:03:43 +0300

    creating a simple library

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertNotEqual(self.calc.add(4, 6), 8)
