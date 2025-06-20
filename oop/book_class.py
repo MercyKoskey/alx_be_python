38c1c31803865298ff561d6584a43f38b8c5e07c
Mercy Koskey <koskey.mercy@gmail.com>
2025-06-20 17:37:08 +0300

    utilizing magic methods and enhancing functionalities

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,10 +3,9 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book created: {self.title} by {self.author}, {self.year}")
 
     def __del__(self):
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         return f"'{self.title}' by {self.author}, published in {self.year}"
